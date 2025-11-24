547bcc7f478c83ae0a972ff7274a91078bdba9ac
Abhi <satpute.abhi22@gmail.com>
2025-11-24 23:03:13 +0530

    Initial Commit

---
+++ FirstIosApp/FirstIosAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstIosAppApp.swift
+//  FirstIosApp
+//
+//  Created by Abhishek Satpute on 24/11/25.
+//
+
+import SwiftUI
+
+@main
+struct FirstIosAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
